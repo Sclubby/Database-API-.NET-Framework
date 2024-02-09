4879bde9f1fbed252f7aec1c743aa226ab4bcae2
Sclubby <seimi2@uic.edu>
2024-02-08 21:35:27 -0600

    Add project files.

---
+++ Controllers/Data/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using FirstAPI;
+using Microsoft.EntityFrameworkCore;
+
+namespace SuperHeroAPI.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options){ }
+
+        public DbSet<SuperHero> SuperHeroes { get; set; }
+    }
+}
